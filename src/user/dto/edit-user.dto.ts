fa6c60142f6d8fe41b6c0c960e6623e7f1660e33
Miguel Duque <miguel.duque@lwmail.com.br>
2023-01-10 11:29:38 -0300

    implements test auth and user

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,15 @@
+import { isEmail, IsString, IsEmail, IsOptional } from 'class-validator';
+
+export class EditUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,148 @@
+import { ValidationPipe, INestApplication } from '@nestjs/common';
+import * as pactum from 'pactum';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { AuthDto } from '../src/auth/dto/auth.dto';
+import { EditUserDto } from '../src/user/dto/edit-user.dto';
+import { PrismaService } from '../src/infra/prisma/prisma.service';
+
+describe('App e2e', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+  describe('Auth', () => {
+    const dto: AuthDto = { email: 'miguel@duquebr.com', password: '123' };
+    describe('Signup', () => {
+      it('Should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('Should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('Should throw if no body provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+      it('Should Signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe('Signin', () => {
+      // let accessToken: string;
+      it('Should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+      it('Should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+      it('Should throw if no body provided', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400);
+      });
+
+      it('Should Signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
+    });
+  });
+  describe('User', () => {
+    describe('Get me', () => {
+      it('Should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: `Bearer $S{userAt}`,
+          })
+          .expectStatus(200);
+        // .inspect();
+      });
+    });
+    describe('Edit user', () => {
+      it('Should Edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Miguel',
+          email: 'miguel@duquebr.com',
+        };
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: `Bearer $S{userAt}`,
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.firstName)
+          .expectBodyContains(dto.email);
+      });
+    });
+  });
+  // describe('Bookmarks', () => {
+  //   describe('Create bookmarks', () => {
+  //     it.todo('Should Create bookmarks');
+  //   });
+  //   describe('Get bookmarks', () => {
+  //     it.todo('Should Get bookmarks');
+  //   });
+  //   describe('Get bookmark by id', () => {
+  //     it.todo('Should Get bookmark by id');
+  //   });
+  //   describe('Edit bookmark', () => {
+  //     it.todo('Should Edit bookmark by id');
+  //   });
+  //   describe('Delete bookmark', () => {
+  //     it.todo('Should Delete bookmark by id');
+  //   });
+  // });
+});
